049cfe9de113491af793da94346f1323090a5114
Christoph Thiede <christoph.thiede@student.hpi.de>
2021-07-08 23:53:10 +0200

    Fix headless VM execution
    See https://github.com/squeak-smalltalk/squeak-app/pull/17#issuecomment-876753284

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -8,14 +8,22 @@ buildAio="$build-All-in-One.zip"
 wget "http://files.squeak.org/trunk/$build/$buildAio"
 unzip -d allInOne/ "$buildAio" -x '*.mo'  # skip superfluous local files
 
+# Prepare VM execution
 export script_dir
 script_dir="$(realpath "$(dirname "$0")")"
 cd allInOne || exit 1
-# Hack: Make squeak.sh capable of passing arguments to the image
+# HACK: Modify squeak.sh because arguments are currently not available for the All-in-One bundles
+# See: https://github.com/squeak-smalltalk/squeak-app/pull/17#issuecomment-876753284
+# Make squeak.sh capable of passing arguments to the image
 sed -i '$s/$/ "$@"/' squeak.sh
+if [[ "$CI" == true ]]; then
+	# Add -headless flag to the VM configuration
+	# shellcheck disable=SC2016
+	sed -ie 's/\(exec "${VM}"\)\( "${IMAGE}"\)/\1 -headless\2/' squeak.sh
+fi
 
 # Prepare image (install repository)
-./squeak.sh -headless "$script_dir/prepareImage.st"
+./squeak.sh "$script_dir/prepareImage.st"
 
 # Clean up caches
 shopt -s globstar
